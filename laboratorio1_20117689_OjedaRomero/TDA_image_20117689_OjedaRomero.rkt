25510b9a1405b4a58b9ce4f3912cdf73a8c1291f
Gabriel Ojeda Romero <gabriel.ojeda@usach.cl>
2022-09-05 12:03:53 -0400

    Selectores funcion get
    Se agregaron los selectores para extraer el ancho y largo de una imgaen, ademas de comentarios.

--- laboratorio1_20117689_OjedaRomero/TDA_image_20117689_OjedaRomero.rkt
+++ laboratorio1_20117689_OjedaRomero/TDA_image_20117689_OjedaRomero.rkt
@@ -1,9 +1,20 @@
 #lang racket
 (provide (all-defined-out))
 
+;---------- TDA IMAGE ----------.
 
+;----- Constructor -----.
 (define (image width height . pixel )
   (list width height pixel))
 
   
+;----- Selectores -----.
+(define (get-width lista) (car image))
 
+(define (get-height lista) (cadr image))
+
+
+
+
+
+;----- Modificadores -----.
