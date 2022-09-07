8688a2454476a16d9f4641ae7eb3c1c4b2d41656
Gabriel Ojeda Romero <gabriel.ojeda@usach.cl>
2022-09-06 23:00:32 -0400

    Modificadores y Selectores del TDA image
    Se agregaron los modificadores para el ancho y largo de la imagen, y se corregieron los selectores, ademas se agrego el dominio y recorrido en comentarios de cada uno de los selectores y modificadores.

--- laboratorio1_20117689_OjedaRomero/TDA_image_20117689_OjedaRomero.rkt
+++ laboratorio1_20117689_OjedaRomero/TDA_image_20117689_OjedaRomero.rkt
@@ -4,17 +4,42 @@
 ;---------- TDA IMAGE ----------.
 
 ;----- Constructor -----.
-(define (image width height . pixel )
-  (list width height pixel))
+
+(define (image width height . pixels )
+  (list width height pixels))
 
   
-;----- Selectores -----.
-(define (get-width lista) (car image))
+;----- Selectores (GET)-----.
+;Selector del element width, ancho de la imagen.
+;Dom: Lista
+;Rec: numero (int)
+(define (get-width lista) (car lista))
 
-(define (get-height lista) (cadr image))
+;Selector del element height, largo de la imagen.
+;Dom: Lista
+;Rec: numero (int)
+(define (get-height lista) (cadr lista))
+
+;Selector del element pixel, pixel de la iamgen.
+;Dom: Lista
+;Rec: Lista
+(define (get-pixels lista) (caddr lista))
 
 
 
+;----- Modificadores (SET) -----.
+;Modificador del element width, ancho de la imagen.
+;Dom: Lista x lista
+;Rec: Lista
+(define (set-width mi-image new-width)
+  (image new-width
+         (get-height mi-image)
+         (get-pixels mi-image)))
 
-
-;----- Modificadores -----.
+;Modificador del element height, largo de la imagen.
+;Dom: Lista x lista
+;Rec: Lista
+(define (set-height mi-image new-height)
+  (image (get-width mi-image)
+         new-height
+         (get-pixels mi-image)))
