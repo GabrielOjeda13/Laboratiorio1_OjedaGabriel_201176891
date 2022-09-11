f7cfe16c9e137629e3cf7fdd60e346232718a437
Gabriel Ojeda Romero <gabriel.ojeda@usach.cl>
2022-09-11 02:13:50 -0300

    Modificadores TDA Pixbit
    Se agregaron 2 modificadores (set) para el tda pixbit, una para la coordenada x y otro para la coordenada y, ambos con sus respectivos comentarios.

--- laboratorio1_20117689_OjedaRomero/TDA_pixbit_20117689_OjedaRomero.rkt
+++ laboratorio1_20117689_OjedaRomero/TDA_pixbit_20117689_OjedaRomero.rkt
@@ -25,3 +25,23 @@
 ;Dom: Lista
 ;Rec: numero (int)
 (define (get-d lista) (cadddr lista))
+
+
+;----- Modificadores (SET) -----.
+;Modificador del element x, cordenada x del pixbit.
+;Dom: Lista x lista
+;Rec: Lista
+(define (set-x mi-pixbit new-x)
+  (pixbit-d new-x
+         (get-y mi-pixbit)
+         (get-bit mi-pixbit)
+         (get-d mi-pixbit)))
+
+;Modificador del element y, cordenada y del pixbit.
+;Dom: Lista x lista
+;Rec: Lista
+(define (set-y mi-pixbit new-y)
+  (pixbit-d (get-x mi-pixbit)
+            new-y
+            (get-bit mi-pixbit)
+            (get-d mi-pixbit)))
