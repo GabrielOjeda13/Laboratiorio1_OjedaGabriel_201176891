82c637c8d95cc8b79cb8f4d8558b56f6b2123f46
Gabriel Ojeda Romero <gabriel.ojeda@usach.cl>
2022-09-17 02:25:03 -0300

    TDA pixrgb-d
    Se creo el nuevo TDA pixrgb-d junto a los selectores y modificadores correspondientes solo a las variables R G y B, dado que los selectores y modificadores asociados a las coordenadas X e Y junto a la profundidad D, se repiten en el otro TDA, por lo tanto estas 3 ultimas variables son transversales para los 3 TDA de pixeles, por ende se desarrollan solo 1 vez y se utiliza en los 3.

---
+++ laboratorio1_20117689_OjedaRomero/TDA_pixrgb_20117689_OjedaRomero.rkt
@@ -0,0 +1,70 @@
+#lang racket
+;----- Constructor -----.
+(define (pixrgb-d x y r g b d)
+  (list x y r g b d))
+
+;----- Selectores (GET)-----.
+;Selector del element x, cordenada x del pixrgb.
+;Dom: Lista
+;Rec: numero (int)
+;(define (get-x lista) (car lista))
+
+;Selector del element y, cordenada y del pixrgb.
+;Dom: Lista
+;Rec: numero (int)
+;(define (get-y lista) (cadr lista))
+
+;Selector del element r, numero del 0 al 255 del pixrgb.
+;Dom: Lista
+;Rec: numero (int)
+(define (get-r lista) (cadr lista))
+
+;Selector del element g, numero del 0 al 255 del pixrgb.
+;Dom: Lista
+;Rec: numero (int)
+(define (get-g lista) (cadr lista))
+
+;Selector del element b, numero del 0 al 255 del pixrgb.
+;Dom: Lista
+;Rec: numero (int)
+(define (get-b lista) (cadr lista))
+
+;Selector del element d, deph (profundidad) del pixrgb.
+;Dom: Lista
+;Rec: numero (int)
+;(define (get-d lista) (cadddr lista))
+
+
+;----- Modificadores (SET) -----.
+;Modificador del element r, numero del 0 al 255 del pixrgb.
+;Dom: Lista x lista
+;Rec: Lista
+(define (set-r mi-pixrgb new-r)
+  (pixrgb-d (get-x mi-pixrgb)
+            (get-y mi-pixrgb)
+            new-r
+            (get-g mi-pixrgb)
+            (get-b mi-pixrgb)
+            (get-d mi-pixrgb)))
+
+;Modificador del element g, numero del 0 al 255 del pixrgb.
+;Dom: Lista x lista
+;Rec: Lista
+(define (set-g mi-pixrgb new-g)
+  (pixrgb-d (get-x mi-pixrgb)
+            (get-y mi-pixrgb)
+            (get-r mi-pixrgb)
+            new-g
+            (get-b mi-pixrgb)
+            (get-d mi-pixrgb)))
+
+;Modificador del element b, numero del 0 al 255 del pixrgb.
+;Dom: Lista x lista
+;Rec: Lista
+(define (set-b mi-pixrgb new-b)
+  (pixrgb-d (get-x mi-pixrgb)
+            (get-y mi-pixrgb)
+            (get-r mi-pixrgb)
+            (get-g mi-pixrgb)
+            new-b
+            (get-d mi-pixrgb)))
